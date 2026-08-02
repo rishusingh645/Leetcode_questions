e345737fcaae4571f53ef3c542784eaed8e3b0e0
Rishu Raj <1857.rishuraj@gmail.com>
2026-08-02 18:44:48 +0530

    Time: 0 ms (100%), Space: 10.1 MB (11.6%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        unordered_map <char,int> m;
+        for (char c : s)
+            m[c]++;
+        for (char c : t)
+        {
+            m[c]--;
+            if (m[c] < 0)
+                return false;
+        }
+        return true;
+    }
+};
