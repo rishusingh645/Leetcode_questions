49458562307f742633f8446507d6be57d55ac20a
Rishu Raj <1857.rishuraj@gmail.com>
2026-08-06 14:51:08 +0530

    Time: 9 ms (49.72%), Space: 44.8 MB (48.92%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int count = 0, i = 0, j = 0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        while (i < g.size() && j < s.size())
+        {
+            if (g[i] <= s[j])
+            {
+                i++;
+                j++;
+                count++;
+            }
+            else
+                j++;
+        }
+        return count;
+    }
+};
